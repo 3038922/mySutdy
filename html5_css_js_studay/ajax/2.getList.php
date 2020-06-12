3a2957dfe0a598870573bf7f42d843d3334db7b0
3038922 <31612534@qq.com>
2020-06-12 14:56:32 +0800

    update
    Signed-off-by: 3038922 <31612534@qq.com>

---
+++ html5_css_js_studay/ajax/2.getList.php
@@ -0,0 +1,9 @@
+<?php
+header('content-type:text/html;charset="utf-8"');
+error_reporting(0);
+// 关联数组
+$arr2=array('username'=>'小屁屁',
+            'age'=>12,
+            'sex'=>'男');
+echo json_encode($arr2);
+?>
