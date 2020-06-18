a55eb42f943c8f63c985b1970c7aae6fedc37ba8
3038922 <31612534@qq.com>
2020-06-18 17:06:56 +0800

    upate
    Signed-off-by: 3038922 <31612534@qq.com>

---
+++ webDev_study/vuesutdy/src/user.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>hello vue</div>
+  <input type="text" ref="mytext" />
+  <button>add</button>
+  <ul>
+    <li v-for="data in datalist" :key="data">{{data}}</li>
+  </ul>
+</template>
+
+<script>
+// ES6 导出
+export default {
+  data(){
+    return {  
+      datalist:[]
+    }
+  },
+  methods: {
+    handleAdd() {
+      this.datalist.push(this.)
+      console.log("add11111",this.$refs.mytext.value);
+    }
+  },
+};
+</script>
