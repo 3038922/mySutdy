967103a9126ec3a4177e36cd8382918a8944590c
3038922 <31612534@qq.com>
2020-06-22 23:21:01 +0800

    update
    Signed-off-by: 3038922 <31612534@qq.com>

--- webDev_study/django/App/views.py
+++ webDev_study/django/App/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 # 导入 HttpResponse
 from django.http import HttpResponse
 # Create your views here.
+from App.models import User
 
 
 def home(request):
-    return render(request, "index.html", context={"title": "Django", "name": "guguji"})
+    # 查询数据库
+    users = User.objects.all()
+    return render(request, "index.html", context={"title": "Django", "name": "挂机", "users": users})
