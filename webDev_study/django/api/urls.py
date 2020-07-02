a6bdd12d7c645bf482aaec92273efb2da7957217
3038922 <31612534@qq.com>
2020-07-02 15:10:46 +0800

    update
    Signed-off-by: 3038922 <31612534@qq.com>

--- webDev_study/django/api/urls.py
+++ webDev_study/django/api/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-    url(r'books/', views.Book.as_view()),
+    url(r'^books/$', views.Book.as_view()),
+    url(r'^books/(?P<pk>.*)/$', views.Book.as_view()),
 ]
--- webDev_study/django/api/views.py
+++ webDev_study/django/api/views.py
@@ -1,11 +1,44 @@
 from django.http import JsonResponse
 from django.views import View
+from . import models
 
 
 # Create your views here.
+# 六大基础接口 获取一个 获取所有 增加一个 删除一个 整体更新一个 局部更新一个
+# 十大接口 群增 群删 整体群改 局部改 群改
 class Book(View):
     def get(self, request, *args, **kwargs):
-        return JsonResponse('get ok', safe=False)
+        pk = kwargs.get('pk')
+        if not pk:  # 群查
+            bookList = []
+            for it in models.Book.objects.all():
+                dic = {}
+                dic['title'] = it.title
+                dic['price'] = it.price
+                bookList.append(dic)
+            print("群查:", bookList)
+            return JsonResponse({
+                'status': 0,
+                'msg': 'ok',
+                'results': bookList
+            },
+                                json_dumps_params={'ensure_ascii': False})
+        else:
+            bookDic = models.Book.objects.filter(pk=pk).values(
+                'title', 'price').first()
+            if bookDic:
+                return JsonResponse(
+                    {
+                        'status': 0,
+                        'msg': 'ok',
+                        'results': bookDic
+                    },
+                    json_dumps_params={'ensure_ascii': False})
+            return JsonResponse({
+                'status': 2,
+                'msg': '无结果',
+            },
+                                json_dumps_params={'ensure_ascii': False})
 
     def post(self, request, *args, **kwargs):
         return JsonResponse('post ok', safe=False)
