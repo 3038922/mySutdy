472fb57510b39eb40e4e6edddef6f2ebca4a0a74
3038922 <31612534@qq.com>
2020-07-18 00:39:32 +0800

    update
    Signed-off-by: 3038922 <31612534@qq.com>

--- webDev_study/ncrBlog/frontend_vue/vue.config.js
+++ webDev_study/ncrBlog/frontend_vue/vue.config.js
@@ -1,12 +1,19 @@
 // // 反向代理
+// module.exports = {
+//   devServer: {
+//     proxy: {
+//       '/json': {
+//         target: 'http://10.195.106.43:11111',
+//         // ws: true,
+//         changeOrigin: true
+//       }
+//     }
+//   }
+// }
+
+//端口修改
 module.exports = {
   devServer: {
-    proxy: {
-      '/json': {
-        target: 'http://10.195.106.43:11111',
-        // ws: true,
-        changeOrigin: true
-      }
-    }
+    port: 11111
   }
 }
