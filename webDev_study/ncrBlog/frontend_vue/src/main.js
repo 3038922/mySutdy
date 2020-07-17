7b9473da13c182e35cdc67755f68790c9858f303
3038922 <31612534@qq.com>
2020-07-18 01:24:45 +0800

    配置完成
    Signed-off-by: 3038922 <31612534@qq.com>

--- webDev_study/ncrBlog/frontend_vue/src/main.js
+++ webDev_study/ncrBlog/frontend_vue/src/main.js
@@ -1,13 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
-import store from "./store";
-import router from "./router";
-import "./plugins/element.js";
+import Vue from 'vue'
+import ElementUI from 'element-ui' // 导入ELEMENTUI
+import App from './App.vue'
+import store from './store'
+import router from './router'
+import './plugins/element.js'
+import 'element-ui/lib/theme-chalk/index.css' //导入样式
 
-Vue.config.productionTip = false;
+Vue.use(ElementUI)
+Vue.config.productionTip = false
 
 new Vue({
   store,
   router,
-  render: h => h(App)
-}).$mount("#app");
+  render: (h) => h(App)
+}).$mount('#app')
