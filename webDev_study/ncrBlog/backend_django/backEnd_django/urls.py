102b4a10584c37728757b412042e6ee798518ccd
3038922 <31612534@qq.com>
2020-07-18 15:06:34 +0800

    update
    Signed-off-by: 3038922 <31612534@qq.com>

--- webDev_study/ncrBlog/backend_django/backEnd_django/urls.py
+++ webDev_study/ncrBlog/backend_django/backEnd_django/urls.py
@@ -14,16 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import include, path
-#from rest_framework import routers
-# from api import views
-# router = routers.DefaultRouter()
-# router.register(r'users', views.UserViewSet)
-# router.register(r'groups', views.GroupViewSet)
+from rest_framework.response import Response
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
+
+def log(request):
+    return Response('test')
+
+
 urlpatterns = [
-    # path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/', include('api.urls')),
+    path('', log),
 ]
