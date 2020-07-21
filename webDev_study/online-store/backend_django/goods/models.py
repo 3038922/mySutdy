786edb24820329ff988cac4b8212c3a3eff9f41b
3038922 <31612534@qq.com>
2020-07-21 23:55:32 +0800

    可以启动了
    Signed-off-by: 3038922 <31612534@qq.com>

--- webDev_study/online-store/backend_django/goods/models.py
+++ webDev_study/online-store/backend_django/goods/models.py
@@ -14,10 +14,19 @@ class GoodsCategory(models.Model):
         (3, "三级类目"),
     )
     name = models.CharField(default="", max_length=30, verbose_name="类别名", help_text="类别名")  # 名字
-    code = models.CharField(default="", max_length=30, verbose_name="类别code", help_text="类别code")  # 类编码
+    code = models.CharField(default="", max_length=30, verbose_name="类别code",
+                            help_text="类别code")  # 类编码
     desc = models.TextField(default="", max_length=200, verbose_name="类别描述", help_text="类别描述")  #
-    category_type = models.CharField(choices=CATEGORY_TYPE, verbose_name="类目级别", help_text="类目级别")  #什么级别的类
-    parent_category = models.ForeignKey("self", null=True, blank=True, verbose_name="父目录级别", help_text="父目录", related_name="sub_cat",
+    category_type = models.CharField(max_length=200,
+                                     choices=CATEGORY_TYPE,
+                                     verbose_name="类目级别",
+                                     help_text="类目级别")  #什么级别的类
+    parent_category = models.ForeignKey("self",
+                                        null=True,
+                                        blank=True,
+                                        verbose_name="父目录级别",
+                                        help_text="父目录",
+                                        related_name="sub_cat",
                                         on_delete=models.CASCADE)  # 父类别可以为空
     is_tab = models.BooleanField(default=False, verbose_name="是否导航", help_text="是否导航")  # 是否显示在tab
     add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
@@ -36,7 +45,7 @@ class GoodsCategoryBrand(models.Model):
     """
     name = models.CharField(default="", max_length=30, verbose_name="品牌名", help_text="品牌名")  # 名字
     desc = models.TextField(default="", max_length=200, verbose_name="品牌描述", help_text="品牌描述")  #
-    image = models.CharField(max_length=200, upload_to="brand/images/")  # 品牌图片上传的地方
+    image = models.ImageField(max_length=200, upload_to="brand/images/")  # 品牌图片上传的地方
     add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
 
     class Meta:
@@ -51,7 +60,12 @@ class Goods(models.Model):
     """
     商品
     """
-    category = models.ForeignKey(GoodsCategory, null=True, blank=True, verbose_name="商品类目", help_text="商品类目", on_delete=models.CASCADE)
+    category = models.ForeignKey(GoodsCategory,
+                                 null=True,
+                                 blank=True,
+                                 verbose_name="商品类目",
+                                 help_text="商品类目",
+                                 on_delete=models.CASCADE)
     goods_sn = models.CharField(max_length=50, default="", verbose_name="商品唯一编号")
     name = models.CharField(default="", max_length=30, verbose_name="商品名称")
     click_num = models.IntegerField(default=0, verbose_name="商品点击数")
@@ -61,7 +75,12 @@ class Goods(models.Model):
     market_price = models.FloatField(default=0, verbose_name="市场价格")
     shop_price = models.FloatField(default=0, verbose_name="本店价格")
     goods_brief = models.TextField(max_length=500, verbose_name="商品简短描述")  # 富文本描述
-    goods_desc = UEditorField(verbose_name=u"内容", imagePath="goods/images/", width=1000, height=300, filePath="goods/files/", default="")
+    goods_desc = UEditorField(verbose_name=u"内容",
+                              imagePath="goods/images/",
+                              width=1000,
+                              height=300,
+                              filePath="goods/files/",
+                              default="")
     ship_free = models.BooleanField(default=True, verbose_name="是否承担运费")  # 是否免运费
     goods_front_image = models.ImageField(upload_to="", null=True, blank=True, verbose_name="封面图")
     is_new = models.BooleanField(default=False, verbose_name="是否新品")  # 是否新品
@@ -81,7 +100,10 @@ class GoodsImage(models.Model):
     一对多关系必须建表了
     商品轮播图
     """
-    goods = models.ForeignKey(Goods, verbose_name="商品", related_name="images", on_delete=models.CASCADE)
+    goods = models.ForeignKey(Goods,
+                              verbose_name="商品",
+                              related_name="images",
+                              on_delete=models.CASCADE)
     image = models.ImageField(upload_to="", null=True, blank=True, verbose_name="图片")
     image_url = models.CharField(max_length=300, null=True, blank=True, verbose_name="图片url")
     add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
