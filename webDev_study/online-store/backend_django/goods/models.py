00a44c32e81e3beb8f26ce842738e76d5ed617e4
3038922 <31612534@qq.com>
2020-07-21 23:29:17 +0800

    update
    Signed-off-by: 3038922 <31612534@qq.com>

--- webDev_study/online-store/backend_django/goods/models.py
+++ webDev_study/online-store/backend_django/goods/models.py
@@ -13,31 +13,13 @@ class GoodsCategory(models.Model):
         (2, "二级类目"),
         (3, "三级类目"),
     )
-    name = models.CharField(default="",
-                            max_length=30,
-                            verbose_name="类别名",
-                            help_text="类别名")  # 名字
-    code = models.CharField(default="",
-                            max_length=30,
-                            verbose_name="类别code",
-                            help_text="类别code")  # 类编码
-    desc = models.TextField(default="",
-                            max_length=200,
-                            verbose_name="类别描述",
-                            help_text="类别描述")  #
-    category_type = models.CharField(choices=CATEGORY_TYPE,
-                                     verbose_name="类目级别",
-                                     help_text="类目级别")  #什么级别的类
-    parent_category = models.ForeignKey("self",
-                                        null=True,
-                                        blank=True,
-                                        verbose_name="父目录级别",
-                                        help_text="父目录",
-                                        related_name="sub_cat",
+    name = models.CharField(default="", max_length=30, verbose_name="类别名", help_text="类别名")  # 名字
+    code = models.CharField(default="", max_length=30, verbose_name="类别code", help_text="类别code")  # 类编码
+    desc = models.TextField(default="", max_length=200, verbose_name="类别描述", help_text="类别描述")  #
+    category_type = models.CharField(choices=CATEGORY_TYPE, verbose_name="类目级别", help_text="类目级别")  #什么级别的类
+    parent_category = models.ForeignKey("self", null=True, blank=True, verbose_name="父目录级别", help_text="父目录", related_name="sub_cat",
                                         on_delete=models.CASCADE)  # 父类别可以为空
-    is_tab = models.BooleanField(default=False,
-                                 verbose_name="是否导航",
-                                 help_text="是否导航")  # 是否显示在tab
+    is_tab = models.BooleanField(default=False, verbose_name="是否导航", help_text="是否导航")  # 是否显示在tab
     add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
 
     class Meta:
@@ -52,16 +34,9 @@ class GoodsCategoryBrand(models.Model):
     """
     品牌名
     """
-    name = models.CharField(default="",
-                            max_length=30,
-                            verbose_name="品牌名",
-                            help_text="品牌名")  # 名字
-    desc = models.TextField(default="",
-                            max_length=200,
-                            verbose_name="品牌描述",
-                            help_text="品牌描述")  #
-    image = models.CharField(max_length=200,
-                             upload_to="brand/images/")  # 品牌图片上传的地方
+    name = models.CharField(default="", max_length=30, verbose_name="品牌名", help_text="品牌名")  # 名字
+    desc = models.TextField(default="", max_length=200, verbose_name="品牌描述", help_text="品牌描述")  #
+    image = models.CharField(max_length=200, upload_to="brand/images/")  # 品牌图片上传的地方
     add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
 
     class Meta:
@@ -76,15 +51,8 @@ class Goods(models.Model):
     """
     商品
     """
-    category = models.ForeignKey(GoodsCategory,
-                                 null=True,
-                                 blank=True,
-                                 verbose_name="商品类目",
-                                 help_text="商品类目",
-                                 on_delete=models.CASCADE)
-    goods_sn = models.CharField(max_length=50,
-                                default="",
-                                verbose_name="商品唯一编号")
+    category = models.ForeignKey(GoodsCategory, null=True, blank=True, verbose_name="商品类目", help_text="商品类目", on_delete=models.CASCADE)
+    goods_sn = models.CharField(max_length=50, default="", verbose_name="商品唯一编号")
     name = models.CharField(default="", max_length=30, verbose_name="商品名称")
     click_num = models.IntegerField(default=0, verbose_name="商品点击数")
     sold_num = models.IntegerField(default=0, verbose_name="商品销售量")
@@ -92,20 +60,10 @@ class Goods(models.Model):
     goods_num = models.IntegerField(default=0, verbose_name="商品库存数")  # 库存数量
     market_price = models.FloatField(default=0, verbose_name="市场价格")
     shop_price = models.FloatField(default=0, verbose_name="本店价格")
-    goods_brief = models.TextField(max_length=500,
-                                   verbose_name="商品简短描述")  # 富文本描述
-    goods_desc = UEditorField(verbose_name=u"内容",
-                              imagePath="goods/images/",
-                              width=1000,
-                              height=300,
-                              filePath="goods/files/",
-                              default="")
-    ship_free = models.BooleanField(default=True,
-                                    verbose_name="是否承担运费")  # 是否免运费
-    goods_front_image = models.ImageField(upload_to="",
-                                          null=True,
-                                          blank=True,
-                                          verbose_name="封面图")
+    goods_brief = models.TextField(max_length=500, verbose_name="商品简短描述")  # 富文本描述
+    goods_desc = UEditorField(verbose_name=u"内容", imagePath="goods/images/", width=1000, height=300, filePath="goods/files/", default="")
+    ship_free = models.BooleanField(default=True, verbose_name="是否承担运费")  # 是否免运费
+    goods_front_image = models.ImageField(upload_to="", null=True, blank=True, verbose_name="封面图")
     is_new = models.BooleanField(default=False, verbose_name="是否新品")  # 是否新品
     is_hot = models.BooleanField(default=False, verbose_name="是否热卖")  # 是否热卖
     add_time = models.DateTimeField(default=datetime.now, verbose_name="商品添加时间")
@@ -123,18 +81,9 @@ class GoodsImage(models.Model):
     一对多关系必须建表了
     商品轮播图
     """
-    goods = models.ForeignKey(Goods,
-                              verbose_name="商品",
-                              related_name="images",
-                              on_delete=models.CASCADE)
-    image = models.ImageField(upload_to="",
-                              null=True,
-                              blank=True,
-                              verbose_name="图片")
-    image_url = models.CharField(max_length=300,
-                                 null=True,
-                                 blank=True,
-                                 verbose_name="图片url")
+    goods = models.ForeignKey(Goods, verbose_name="商品", related_name="images", on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="", null=True, blank=True, verbose_name="图片")
+    image_url = models.CharField(max_length=300, null=True, blank=True, verbose_name="图片url")
     add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
 
     class Meta:
@@ -149,9 +98,7 @@ class Banner(models.Model):
     """
     轮播的商品
     """
-    goods = models.ForeignKey(Goods,
-                              verbose_name="商品",
-                              on_delete=models.CASCADE)
+    goods = models.ForeignKey(Goods, verbose_name="商品", on_delete=models.CASCADE)
     image = models.ImageField(upload_to="banner", verbose_name="轮播图片")
     index = models.IntegerField(default=0, verbose_name="轮播顺序")
     add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
--- webDev_study/online-store/backend_django/users/models.py
+++ webDev_study/online-store/backend_django/users/models.py
@@ -9,15 +9,9 @@ class UserProfile(AbstractUser):
     """
     email 有了.
     """
-    name = models.CharField(max_length=30,
-                            null=True,
-                            blank=True,
-                            verbose_name="姓名")
+    name = models.CharField(max_length=30, null=True, blank=True, verbose_name="姓名")
     birthday = models.DateField(null=True, blank=True, verbose_name="出生年月")
-    gender = models.CharField(max_length=6,
-                              choices=(("male", u"男"), ("female", "女")),
-                              default="male",
-                              verbose_name="性别")  # 性别
+    gender = models.CharField(max_length=6, choices=(("male", u"男"), ("female", "女")), default="male", verbose_name="性别")  # 性别
     mobile = models.CharField(max_length=11, verbose_name="电话")
 
     class Meta:
