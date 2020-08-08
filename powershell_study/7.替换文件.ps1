ae2f516326aa479a1a29d7e20c82bbd6dc16824f
3038922 <31612534@qq.com>
2020-08-09 00:51:23 +0800

    update
    Signed-off-by: 3038922 <31612534@qq.com>

--- powershell_study/7.替换文件.ps1
+++ powershell_study/7.替换文件.ps1
@@ -2,4 +2,16 @@
 $targetPath = "C:\Users\$env:UserName\AppData\Local\Programs\Python\Python38\Lib\site-packages\pros\common\ui\"
 $tmpFileName = '.\__init__.py'
 Write-Host  $targetPath -ForegroundColor Green
-Move-Item -Path $tmpFileName -Destination $targetPath -Force
+Copy-Item -Path $tmpFileName -Destination $targetPath -Force
+
+$targetPath = "C:\Users\$env:UserName\AppData\Roaming\PROS\templates"
+$tmpFileName = '.\kernel@3.2.1'
+Write-Host  $targetPath -ForegroundColor Green
+Copy-Item -Path $tmpFileName -Destination $targetPath -Recurse -Force
+
+$targetPath = "C:\Users\$env:UserName\AppData\Roaming\PROS\templates"
+$tmpFileName = '.\okapilib@4.0.4'
+Write-Host  $targetPath -ForegroundColor Green
+Copy-Item -Path $tmpFileName -Destination $targetPath -Recurse -Force
+
+# Expand-Archive -Path .\extern_script.zip -DestinationPath  .\ -Force
