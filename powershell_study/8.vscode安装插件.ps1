d8b64fdde7f79a206e6016ba1e8288dc5caa2655
3038922 <31612534@qq.com>
2020-08-09 23:22:25 +0800

    update
    Signed-off-by: 3038922 <31612534@qq.com>

--- powershell_study/8.vscode安装插件.ps1
+++ powershell_study/8.vscode安装插件.ps1
@@ -1,3 +1,15 @@
 ﻿# Write-Host  "正在安装vscode插件sync" -ForegroundColor Green
 # & code --install-extension shan.code-settings-sync
-& vscode.commands.executeCommand('命令放置处');
+
+# "sync.gist": "6c091a7b4ddcb213e72d430dac23422f",
+
+$newfile = @();
+Get-Content C:\Users\$env:UserName\AppData\Roaming\Code\User\settings.json | #读取文件
+% {
+    $newfile += $_;
+    if ($_ -like '{') {
+        #找到匹配的字符串
+        $newfile += '  "sync.gist": "6c091a7b4ddcb213e72d430dac23422f",' #加入新的行
+    }
+}
+$newfile | Out-File test.json #输出
