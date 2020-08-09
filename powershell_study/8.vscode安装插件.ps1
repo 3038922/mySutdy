ac859d55e1245039876bd51fbb61cd3b284d6657
3038922 <31612534@qq.com>
2020-08-10 00:38:52 +0800

    update
    Signed-off-by: 3038922 <31612534@qq.com>

--- powershell_study/8.vscode安装插件.ps1
+++ powershell_study/8.vscode安装插件.ps1
@@ -1,15 +1,6 @@
-﻿# Write-Host  "正在安装vscode插件sync" -ForegroundColor Green
-# & code --install-extension shan.code-settings-sync
+﻿
 
 # "sync.gist": "6c091a7b4ddcb213e72d430dac23422f",
+#C:\Users\$env:UserName\AppData\Roaming\Code\User\settings.json
 
-$newfile = @();
-Get-Content C:\Users\$env:UserName\AppData\Roaming\Code\User\settings.json | #读取文件
-% {
-    $newfile += $_;
-    if ($_ -like '{') {
-        #找到匹配的字符串
-        $newfile += '  "sync.gist": "6c091a7b4ddcb213e72d430dac23422f",' #加入新的行
-    }
-}
-$newfile | Out-File test.json #输出
+& code vscode.workspace.getConfiguration().update('vscodePluginDemo.yourName', '前端艺术家', true);
