013ad10cf5ac0737280fb7f13474789b86bbd727
3038922 <31612534@qq.com>
2020-08-11 16:07:51 +0800

    update
    Signed-off-by: 3038922 <31612534@qq.com>

--- powershell_study/8.vscode安装插件.ps1
+++ powershell_study/8.vscode安装插件.ps1
@@ -1,7 +1,28 @@
-
+Ôªø
 
 # "sync.gist": "6c091a7b4ddcb213e72d430dac23422f",
-#C:\Users\$env:UserName\AppData\Roaming\Code\User\settings.json
-Get-Content C:\Users\$env:UserName\AppData\Roaming\Code\User\settings.json -Encoding UTF8 
-# Set-Content ./test.json -value "’‚ «“ª∏ˆ≤‚ ‘ÃÌº”ƒ⁄»›" -Encoding UTF8
-# Add-Content ./test.json -value "’‚ «“ª∏ˆ≤‚ ‘ÃÌº”ƒ⁄»›" -Encoding UTF8
+# C:\Users\$env:UserName\AppData\Roaming\Code\User\settings.json
+# $json = ConvertFrom-Json($json)
+# $json = ConvertFrom-Json( Get-Content( .\in.json -Encoding UTF8 ))
+# # $json = ConvertFrom-Json -InputObject $json
+# # $json | add-member -Name "sync.gist" -value "1234aaa" -MemberType NoteProperty -force
+# write-host (ConvertTo-Json $json)
+# $json = Get-Content -path .\in.json -Encoding UTF8 
+
+$json = Get-Content C:\Users\$env:UserName\AppData\Roaming\Code\User\settings.json -Encoding UTF8 
+# Ê≥®ÊÑèÊç¢Ë°å
+$json[0] += '
+  "sync.gist": "6c091a7b4ddcb213e72d430dac23422f",'
+Set-Content .\test.json -value $json -Encoding UTF8
+
+# $settingsPath = ".\in.json";
+# $colorSetting = 'workbench.colorTheme';
+# $fontSetting = 'editor.fontSize';
+
+# $data = Get-Content -Raw -Path $settingsPath -ErrorAction silentlycontinue | ConvertFrom-Json
+
+# if ($data) {
+#     $data | add-member -Name "sync.gist" -value "1234aaa" -MemberType NoteProperty -force
+# }
+
+# $data | ConvertTo-Json | Out-File $settingsPath -Encoding utf8
