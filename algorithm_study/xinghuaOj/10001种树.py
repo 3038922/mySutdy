99c5e19f2b0d4700fc9425e8f4baa1c744591db7
3038922 <31612534@qq.com>
2020-11-15 00:37:33 +0800

    不知道为啥出错
    Signed-off-by: 3038922 <31612534@qq.com>

--- algorithm_study/xinghuaOj/10001种树.py
+++ algorithm_study/xinghuaOj/10001种树.py
@@ -19,25 +19,27 @@ targetList = []
 for i in range(hopeNums):
     targetList.append(list(map(int, input().split())))
 targetList.sort(key=lambda x: (x[1]))  #从小到大排
-roadList = [False for n in range(targetList[hopeNums - 1][1] + 1)]  #路段似乎得建大点 因为遍历的时候没0点
+roadList = [False for n in range(0, targetList[hopeNums - 1][1] + 1)]  #路段似乎得建大点 因为遍历的时候没0点
 treeCount = 0
-
 for it in targetList:
-    for j in roadList[it[0]:it[1]]:
-        """
-        遍历区间里的点 统计有没树
-        """
-        if (j):  # 如果检测到种树
-            it[2] -= 1  #目标值-=1
     if (it[2] > 0):  #需要目标还没完成 就继续种树
-        for j in range(it[1], it[0] - 1, -1):
+        count = 0
+        for j in roadList[it[0]:it[1]]:
             """
-            如果
+            看看还需要种多少树
             """
-            if (roadList[j] == False):
-                roadList[j] = True  #标记已种树
-                it[2] -= 1  #目标值-=1
-                treeCount += 1
+            if (j):  # 如果检测到种树ss
+                count += 1
+        if (count < it[2]):  #如果已经种的树比需要种的少 继续种
+            for j in range(it[1], it[0] - 1, -1):
+                print("j:", j, roadList[j])
+                """
+                如果
+                """
+                if (roadList[j] == False):
+                    roadList[j] = True  #标记已种树
+                    it[2] -= 1  #目标值-=1
+                    treeCount += 1
 
 for it in targetList:
     print(it)
